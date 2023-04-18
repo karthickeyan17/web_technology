690e89dc6131eaa2c7ebe65341265851467f18bd
karthickeyan17 <105485904+karthickeyan17@users.noreply.github.com>
2023-04-18 23:36:02 +0530

    Add files via upload

---
+++ Exp7.js
@@ -0,0 +1,40 @@
+calc = require("./calci");
+http = require('http');
+url = require('url');
+querystring = require('querystring');
+
+function onRequest(request, response) {
+    var path = url.parse(request.url).pathname;
+    console.log('Request for ' + path + 'received.');
+    var query = url.parse(request.url).query;
+    console.log(query);
+    var a = Number(querystring.parse(query)["a"]);
+    var b = Number(querystring.parse(query)["b"]);
+    var option = querystring.parse(query)["option"];
+    switch(option){
+        case "+":
+            var ans = (calc.add(a,b));
+            //console.log(ans);
+            response.write("value is "+ans.toString());
+            break;
+        case "-":
+            var ans = (calc.sub(a,b));
+            response.write("value is "+ans.toString());
+            break;
+        case "*":
+            var ans = (calc.mul(a,b));
+            response.write("value is "+ans.toString());
+            break;
+        case "/":
+            var ans = (calc.div(a,b));
+            response.write("value is "+ans.toString());
+            break;
+        default :
+            response.write("Select the Correct option ");
+            break;    
+    }
+    response.end();
+}
+
+http.createServer(onRequest).listen(4055);
+console.log('Server started..');
